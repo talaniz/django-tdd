5543cb8bfbc3f40c45e0c84e954b3ba8bebe391a
sandwichheat <antonio.alaniz@gmail.com>
2015-09-05 23:58:22 -0400

    Added a URL for creating a POST request.

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from lists import views
 
 urlpatterns = [
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
 ]
