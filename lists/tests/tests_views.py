cd108e5785c36960d20e4537593b061f72ae2efb
sandwichheat <antonio.alaniz@gmail.com>
2015-09-19 22:10:49 -0700

    Refactor list view to handle new item POSTS

--- lists/tests/tests_views.py
+++ lists/tests/tests_views.py
@@ -75,15 +75,13 @@ class NewListTest(TestCase):
         self.client.post('/lists/new', data={'item_text':''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
-        
-class NewItemTest(TestCase):
     
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
         
         self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
+            '/lists/%d/' % (correct_list.id,),
             data={'item_text': 'A new item for an existing list'}
         )
         
@@ -97,7 +95,7 @@ class NewItemTest(TestCase):
         correct_list = List.objects.create()
         
         response = self.client.post(
-                        '/lists/%d/add_item' % (correct_list.id),
+                        '/lists/%d/' % (correct_list.id),
                         data={'item_text': 'A new item for an existing list'}
         )
         
--- lists/urls.py
+++ lists/urls.py
@@ -4,5 +4,4 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^new$', 'lists.views.new_list', name='new_list'),
     url(r'^(\d+)/$', 'lists.views.view_list', name="view_list"),
-    url(r'^(\d+)/add_item$', 'lists.views.add_item', name="add_item"),
 ]
