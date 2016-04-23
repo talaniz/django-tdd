48a288591e2f99cc71bdd07f3532b2b1743856a1
sandwichheat <antonio.alaniz@gmail.com>
2016-04-23 00:10:37 -0700

    Use form in all views, back to working state

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -47,7 +47,7 @@ class NewVisitorTest(FunctionalTest):
         # Now a new user, Francis, comes along to the site
         
         ## We use a new browser session to make sure that no information
-        ## of Edith's is coming thorugh from cookies etc
+        ## of Edith's is coming through from cookies etc
         self.browser.refresh()
         self.browser.quit()
         self.browser = webdriver.Firefox()
