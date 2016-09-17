6a5523d99f4f06909ff34ef3838ff08d23409f76
sandwichheat <antonio.alaniz@gmail.com>
2016-09-17 10:19:19 -0700

    Completed duplication validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,9 +1,11 @@
-from unittest import skip
-
 from django.utils.html import escape
 from django.test import TestCase
 
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_LIST_ERROR, 
+    ExistingListItemForm, ItemForm
+)
+
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
@@ -73,7 +75,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 class NewListTest(TestCase):
@@ -125,13 +127,12 @@ class NewListTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_LIST_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -140,7 +141,7 @@ class NewListTest(TestCase):
             data={'text': 'textey'}
         )
         
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
